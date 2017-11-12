5999ba01c3e2d4839442e126412f389cc7145d66
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-12 02:09:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class BoardMember {
     return "You will see great things from Scuber."
   }
   sayHi(){
-    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
 class Ceo extends BoardMember {
