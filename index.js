5e5917f5b0058f700fdfdbc2dccb214df9c4bb31
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-12 02:07:17 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+class BoardMember {
+  constructor(name,homeState,training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training 
+    
+  }
+  veto(){
+    return "No i must disagree";
+  }
+  approve(){
+    return "You can do that!"
+    
+  }
+  doCharity(){
+    return "I like to help people."
+  }
+  releasePressStatement(){
+    return "You will see great things from Scuber"
+  }
+  sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+  }
+}
+class Ceo extends BoardMember {
+  constructor() {
+    super(name,homeState,training)
+  }
+  hireEmployee(){
+    return 'Welcome aboard';
+  }
+}
